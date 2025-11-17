6ab92d2261bdb4f3780a207942b6f9b7314c74c2
jehwan <lluns67@gmail.com>
2025-11-17 19:06:10 +0900

    11/17 테스트자바에서 db 테이블 안에 데이터 수정 하는 것까지 p.94

--- scit48_springEx/test_1112/spring13/spring13/src/test/java/net/nocori/spring13/Spring13ApplicationTests.java
+++ scit48_springEx/test_1112/spring13/spring13/src/test/java/net/nocori/spring13/Spring13ApplicationTests.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Spring13ApplicationTests {
@@ -29,23 +30,40 @@ class Spring13ApplicationTests {
 //		q2.setContent("id는 자동으로 생성되나요?");
 //		q2.setCreateDate(LocalDateTime.now());
 //		this.questionRepository.save(q2);
-		
+//
 //		List<Question> all = this.questionRepository.findAll();
 //		assertEquals(2, all.size());
 //
 //		Question q = all.get(0);
 //		assertEquals("sbb가 무엇인가요?", q.getSubject());
-//
+////
 //		Optional<Question> oq = this.questionRepository.findById(1);
 //		if(oq.isPresent()) {
 //			Question q = oq.get();
 //			assertEquals("ssb가 무엇이가요?", q.getSubject());
-
+//
 //		}
-		Question q = this.questionRepository.findBySubject("ssb가 무엇인가요?");
-		assertEquals(1, q.getId());
+////	//
+//		Question q = this.questionRepository.findBySubject("ssb가 무엇이가요?");
+//		assertEquals(1, q.getId());
 		
-	
+		
+		// subject와 content 조회 하기 위한 코드
+//		Question q = this.questionRepository.findBySubjectAndContent("ssb가 무엇이가요?"
+//									, "sbb에 대해서 알고 싶습니다.");
+//		assertEquals(1, q.getId());
+//
+ 			// like 문 사용 아마도....
+//			List<Question> qList = this.questionRepository.findBySubjectLike("ssb%");
+//			Question q = qList.get(0);
+//
+//			assertEquals("ssb가 무엇이가요?", q.getSubject());
+		
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
 	}
 	
 	
