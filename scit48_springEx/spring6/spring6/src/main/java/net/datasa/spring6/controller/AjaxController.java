5591195c87cfa6cc41b740129c80e2cd9b663962
jehwan <lluns67@gmail.com>
2026-01-05 17:52:17 +0900

    0105 json 관련 처음 껀 뭐였더라

---
+++ scit48_springEx/spring6/spring6/src/main/java/net/datasa/spring6/controller/AjaxController.java
@@ -0,0 +1,58 @@
+package net.datasa.spring6.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class AjaxController {
+	/**
+	 * 	테스트 페이지1로 이동
+	 */
+	@GetMapping("ajax1")
+	public String ajax1(){
+		return "ajax1";
+	}
+	
+	/**
+	 *  Ajax 요청에 대한 응답 1
+	 */
+	// 리턴값을 화면(HTML)로 보내지 말고, HTTP 응답 데이터로 그대로 보내달라
+	@ResponseBody
+	@GetMapping("ajaxText1")
+	public void ajaxText1(){
+		log.debug("AjaxController의 ajaxText1() 실행");
+	}
+	@ResponseBody
+	@PostMapping("ajaxText2")
+	public void ajaxText2(@RequestParam("str") String str){
+		log.debug("AjaxContoller의 ajaxText2()에서 출력 {}",str);
+		throw new RuntimeException("서버에서 실행 중 예외 발생");
+	}
+	
+	/**
+	 *  Ajax 요청에 대한 응답 3
+	 */
+	@GetMapping("ajaxTest3")
+	@ResponseBody
+	public String ajaxTest3(){
+		String msg = "서버에서 보내는 메시지";
+		log.debug("AjaxController의 ajaxTest3()에서 보냄: {}", msg);
+		return msg;
+	}
+	
+	@ResponseBody
+	@PostMapping("ajaxTest4")
+	public int ajaxTest4(
+		@RequestParam("num1") int a,
+		@RequestParam("num2") int b
+	){
+		log.debug("ajaxTest4 전달받은 값:  num1= {}, num2={}", a, b);
+		int sum = a + b;
+		return sum;
+	}
+}
